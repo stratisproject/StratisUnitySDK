82f8677d1715a726119d60d36a9969818d55e9a3
noescape00 <NoEscape0@ya.ru>
2021-08-20 14:30:20 +0300

    UI

--- IntegrationExample_RedRunner/Assets/Scripts/SDKIntegrationManager.cs
+++ IntegrationExample_RedRunner/Assets/Scripts/SDKIntegrationManager.cs
@@ -1,14 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+using NBitcoin;
+using Stratis.Sidechains.Networks;
+using Unity3dApi;
 using UnityEngine;
+using Network = NBitcoin.Network;
 
 public class SDKIntegrationManager : MonoBehaviour
 {
+    public string ApiUrl = "http://localhost:44336/";
+
+    public string Mnemonic = "legal door leopard fire attract stove similar response photo prize seminar frown";
+
     public static SDKIntegrationManager Instance { get; private set; }
 
-    void Start()
+    private Network network = new CirrusTest();
+
+    private StratisUnityManager stratisUnityManager;
+
+    private string address;
+
+    async void Start()
     {
         Instance = this;
+
+        Unity3dClient client = new Unity3dClient(ApiUrl);
+
+        Mnemonic mnemonic = new Mnemonic(Mnemonic, Wordlist.English);
+        stratisUnityManager = new StratisUnityManager(client, network, mnemonic);
+        address = stratisUnityManager.GetAddress().ToString();
+        Debug.Log("Your address: " + address);
+
+        decimal balance = await stratisUnityManager.GetBalanceAsync();
+        Debug.Log("Your balance: " + balance);
     }
 
     public void CoinCollected(int newCoinValue)
