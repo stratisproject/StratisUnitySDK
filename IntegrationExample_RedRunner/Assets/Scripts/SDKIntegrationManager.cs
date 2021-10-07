1e76c0c642de519fda3de56b769648f21d5e1df5
noescape00 <NoEscape0@ya.ru>
2021-10-07 13:56:08 +0300

    mint nft and display balance

--- IntegrationExample_RedRunner/Assets/Scripts/SDKIntegrationManager.cs
+++ IntegrationExample_RedRunner/Assets/Scripts/SDKIntegrationManager.cs
@@ -30,6 +30,8 @@ public class SDKIntegrationManager : MonoBehaviour
 
     public string RedRunnerTokenContractAddress = "t778saxw6Xdgs77Z5ePpaFPCZ9bk4oNrPT";
 
+    public string RedRunnerNFTContractAddress = "tA9k1WMEtanRrY3k4oudiswZr4Z3bXkNNn";
+
     public static SDKIntegrationManager Instance { get; private set; }
 
     private Network network = new CirrusTest();
@@ -42,10 +44,15 @@ public class SDKIntegrationManager : MonoBehaviour
 
     private StandartTokenWrapper tokenRRT;
 
+    private NFTWrapper nft;
+
+    private const int NFTContractLogsStartHeight = 2641320;
+
     private decimal straxBalance = -1;
     private ulong rrtBalance = 0;
+    private ulong NFTBalance = 0;
 
-    void Awake()
+    async void Awake()
     {
         Instance = this;
         client = new Unity3dClient(ApiUrl);
@@ -58,7 +65,10 @@ public class SDKIntegrationManager : MonoBehaviour
     {
         Mnemonic m = new Mnemonic(mnemonic, Wordlist.English);
         stratisUnityManager = new StratisUnityManager(client, network, m);
+
         this.tokenRRT = new StandartTokenWrapper(stratisUnityManager, this.RedRunnerTokenContractAddress);
+        this.nft = new NFTWrapper(stratisUnityManager, RedRunnerNFTContractAddress);
+
         address = stratisUnityManager.GetAddress().ToString();
         Debug.Log("Your address: " + address);
         
@@ -74,10 +84,11 @@ public class SDKIntegrationManager : MonoBehaviour
         {
             try
             {
-                straxBalance = await stratisUnityManager.GetBalanceAsync();
-                rrtBalance = await tokenRRT.GetBalanceAsync(address);
+                this.straxBalance = await stratisUnityManager.GetBalanceAsync();
+                this.rrtBalance = await tokenRRT.GetBalanceAsync(address);
+                this.NFTBalance = await this.nft.BalanceOfAsync(this.address);
 
-                Debug.Log("STRAXBalance: " + straxBalance + " | TokenBalance: " + rrtBalance);
+                Debug.Log("STRAXBalance: " + straxBalance + " | TokenBalance: " + rrtBalance + " | NFTBalance: " + NFTBalance);
             }
             catch (Exception e)
             {
@@ -93,6 +104,70 @@ public class SDKIntegrationManager : MonoBehaviour
         {
             this.BalanceRRTText.text = "RRT: " + rrtBalance;
             this.BalanceStraxText.text = "STRAX: " + Math.Round(straxBalance, 2);
+            this.NFTBalanceText.text = "NFT: " + NFTBalance;
+        }, null);
+    }
+    
+    public void PlayerDied(int distance, bool isHighestScore)
+    {
+        Debug.Log("PLAYER DEATH. coins collected = " + GameManager.Singleton.m_Coin.Value + "    distanceRan = " + distance);
+
+        Task.Run(async () =>
+        {
+            var coinsCollected = GameManager.Singleton.m_Coin.Value;
+
+            if (isHighestScore)
+                Debug.Log("New high distance" + distance);
+
+
+            PlayerNotificationResult result =
+                await client.PlayerAchievementsNotificationAsync(address, "NO_NAME", coinsCollected, distance);
+
+            Debug.Log("SEND TOKENS ON DEATH: " + result.SendTokens);
+        });
+    }
+
+    #region UI
+    private void InitializeUI()
+    {
+        this.PopupPanel.SetActive(false);
+        this.Button_NewMnmemonic.onClick.AddListener(NewMnemonic_bnCall);
+        this.Button_SetMnmemonic.onClick.AddListener(SetMnemonic_bnCall);
+
+        Button_CopyAddr.onClick.AddListener(CopyAddress_ButtonCall);
+        RefreshButton.onClick.AddListener(() => this.StartCoroutine(RefreshBalance()));
+
+        SendStraxButton.onClick.AddListener(() => this.StartCoroutine(SendStrax_ButtonCall()));
+        SendRRTButton.onClick.AddListener(() => this.StartCoroutine(SendRRT_ButtonCall()));
+        Button_MintNFT.onClick.AddListener(() => this.StartCoroutine(MintNFT_ButtonCall()));
+
+        PopupPanelOk_Button.onClick.AddListener(() => this.PopupPanel.SetActive(false));
+    }
+
+    private void CopyAddress_ButtonCall()
+    {
+        GUIUtility.systemCopyBuffer = this.address;
+    }
+
+    private void SetMnemonic_bnCall()
+    {
+        InitMnemonic(this.Mnemonic_InputField.text);
+    }
+
+    private void NewMnemonic_bnCall()
+    {
+        Mnemonic mnemonic = new Mnemonic(Wordlist.English, WordCount.Twelve);
+        this.Mnemonic_InputField.text = mnemonic.ToString();
+
+        SetMnemonic_bnCall();
+    }
+
+    private void DisplayPopup(string text)
+    {
+        SynchronizationContext.Current.Post(state =>
+        {
+            this.PopupPanel.SetActive(true);
+            this.PopupPanel_Text.text = text;
         }, null);
     }
 
@@ -186,66 +261,44 @@ public class SDKIntegrationManager : MonoBehaviour
         }
     }
 
-    public void PlayerDied(int distance, bool isHighestScore)
+    private IEnumerator MintNFT_ButtonCall()
     {
-        Debug.Log("PLAYER DEATH. coins collected = " + GameManager.Singleton.m_Coin.Value + "    distanceRan = " + distance);
-
-        Task.Run(async () =>
+        if (straxBalance <= 0)
         {
-            var coinsCollected = GameManager.Singleton.m_Coin.Value;
-
-            if (isHighestScore)
-                Debug.Log("New high distance" + distance);
-
-
-            PlayerNotificationResult result =
-                await client.PlayerAchievementsNotificationAsync(address, "NO_NAME", coinsCollected, distance);
-
-            Debug.Log("SEND TOKENS ON DEATH: " + result.SendTokens);
-        });
-    }
-
-    #region UI
-    private void InitializeUI()
-    {
-        this.PopupPanel.SetActive(false);
-        this.Button_NewMnmemonic.onClick.AddListener(NewMnemonic_bnCall);
-        this.Button_SetMnmemonic.onClick.AddListener(SetMnemonic_bnCall);
-
-        Button_CopyAddr.onClick.AddListener(CopyAddress_ButtonCall);
-        RefreshButton.onClick.AddListener(() => this.StartCoroutine(RefreshBalance()));
-
-        SendStraxButton.onClick.AddListener(() => this.StartCoroutine(SendStrax_ButtonCall()));
-        SendRRTButton.onClick.AddListener(() => this.StartCoroutine(SendRRT_ButtonCall()));
-
-        PopupPanelOk_Button.onClick.AddListener(() => this.PopupPanel.SetActive(false));
-    }
-
-    private void CopyAddress_ButtonCall()
-    {
-        GUIUtility.systemCopyBuffer = this.address;
-    }
-
-    private void SetMnemonic_bnCall()
-    {
-        InitMnemonic(this.Mnemonic_InputField.text);
-    }
-
-    private void NewMnemonic_bnCall()
-    {
-        Mnemonic mnemonic = new Mnemonic(Wordlist.English, WordCount.Twelve);
-        this.Mnemonic_InputField.text = mnemonic.ToString();
-
-        SetMnemonic_bnCall();
-    }
-
-    private void DisplayPopup(string text)
-    {
-        SynchronizationContext.Current.Post(state =>
+            this.DisplayPopup("You need to have some STRAX to mint NFT.");
+        }
+        else
         {
-            this.PopupPanel.SetActive(true);
-            this.PopupPanel_Text.text = text;
-        }, null);
+            string txHash = null;
+            string error = null;
+
+            Task task = Task.Run(async () =>
+            {
+                try
+                {
+                    string txId = await this.nft.MintAsync(this.address);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError(e.ToString());
+                    error = e.ToString();
+                    return;
+                }
+            });
+
+            while (!task.IsCompleted)
+                yield return null;
+
+            if (error != null)
+            {
+                this.DisplayPopup("Error minting NFT: " + error);
+            }
+            else
+            {
+                this.DisplayPopup(string.Format("Transaction {0} to mint NFT was sent.", txHash));
+            }
+        }
     }
+
     #endregion
 }
