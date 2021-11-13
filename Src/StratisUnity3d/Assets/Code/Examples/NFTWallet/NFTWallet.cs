e29ac75303dc91fe3c47111d53ce41cbcc107e2a
noescape00 <NoEscape0@ya.ru>
2021-11-13 20:12:02 +0300

    nft wallet init async

--- Src/StratisUnity3d/Assets/Code/Examples/NFTWallet/NFTWallet.cs
+++ Src/StratisUnity3d/Assets/Code/Examples/NFTWallet/NFTWallet.cs
@@ -1,16 +1,51 @@
+using System;
+using System.Net.Http;
+using System.Net.Sockets;
+using Cysharp.Threading.Tasks;
+using NBitcoin;
+using Stratis.Sidechains.Networks;
+using Unity3dApi;
 using UnityEngine;
+using Network = NBitcoin.Network;
 
 public class NFTWallet : MonoBehaviour
 {
     public static NFTWallet Instance;
-    
+
+    private readonly string apiUrl = "http://localhost:44336/";
+    private readonly Network network = new CirrusTest();
+
+    [HideInInspector]
+    public StratisUnityManager StratisUnityManager;
+
     void Awake()
     {
         Instance = this;
     }
-
-    public void Initialize(string mnemonic)
+    
+    /// <returns><c>true</c> if success.</returns>
+    public async UniTask<bool> InitializeAsync(string mnemonic)
     {
-        // TODO
+        try
+        {
+            this.StratisUnityManager = new StratisUnityManager(new Unity3dClient(apiUrl), network,
+                new Mnemonic(mnemonic, Wordlist.English));
+
+            await this.StratisUnityManager.GetBalanceAsync();
+        }
+        catch (SocketException e)
+        {
+            return false;
+        }
+        catch (HttpRequestException e)
+        {
+            return false;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e);
+        }
+
+        return true;
     }
 }
